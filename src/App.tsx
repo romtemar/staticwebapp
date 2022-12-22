6316bbea066c491b5ee6048336b7a1e7f2d3bed6
rmaik <roman.maik@ratesetter.com>
2022-12-22 13:02:13 +0200

    Commit-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const apimUrl = 'https://rmaik-test-apim.azure-api.net/api/weather';
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=2401b4c393ad70dc5a8e56b9805ba118`;
+
+  const sendRequest = () => {
+    axios.get(apimUrl).then((response) => {
+      console.log(response.data);
+      console.log(response.status);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +21,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={sendRequest}
         >
-          Learn React
-        </a>
+          Send Request
+        </button>
       </header>
     </div>
   );
